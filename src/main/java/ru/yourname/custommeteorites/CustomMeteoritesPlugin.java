df4804adb3bd9ff8955537590e6510d64563ca0e
Krasnyuk Sergey <17615311+Nixend-creator@users.noreply.github.com>
2025-12-03 12:53:39 +0200

    Implement CustomMeteoritesPlugin with core features

---
+++ src/main/java/ru/yourname/custommeteorites/CustomMeteoritesPlugin.java
@@ -0,0 +1,46 @@
+package ru.yourname.custommeteorites;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.yourname.custommeteorites.commands.MeteorCommand;
+import ru.yourname.custommeteorites.config.ConfigManager;
+import ru.yourname.custommeteorites.spawner.MeteoriteSpawner;
+
+public final class CustomMeteoritesPlugin extends JavaPlugin {
+
+    private ConfigManager configManager;
+    private MeteoriteSpawner meteoriteSpawner;
+
+    @Override
+    public void onEnable() {
+        // Инициализация
+        this.configManager = new ConfigManager(this);
+        this.configManager.loadConfig();
+
+        this.meteoriteSpawner = new MeteoriteSpawner(this, configManager);
+
+        // Регистрация команд
+        getCommand("meteor").setExecutor(new MeteorCommand(this));
+
+        // Запуск планировщика метеоритов
+        meteoriteSpawner.startScheduler();
+
+        getLogger().info("Плагин CustomMeteorites включён!");
+    }
+
+    @Override
+    public void onDisable() {
+        if (meteoriteSpawner != null && meteoriteSpawner.getGenerator() != null) {
+            meteoriteSpawner.getGenerator().cancelCleanupTasks(); // Вызов метода отмены задач
+        }
+        getLogger().info("Плагин CustomMeteorites выключен!");
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    // Метод для получения генератора из других классов (например, MeteoriteSpawner)
+    public MeteoriteGenerator getGenerator() {
+        return meteoriteSpawner != null ? meteoriteSpawner.getGenerator() : null;
+    }
+}
